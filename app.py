4e1e783adcfc07a0b6d0f329d89d8ed065d2dc1b
Abdulrahman <abdulrahamanbabatunde12@gmail.com>
2025-03-06 15:45:35 +0100

    add html

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from model import EvacuationPlanner
 
-app = Flask(__name__)
 planner = EvacuationPlanner()
+
+app = Flask(__name__)
+
+@app.route('/')
+def serve_html():
+    return send_from_directory('.', 'index.html')
 
 @app.route('/plan', methods=['POST'])
 def get_plan():
     data = request.json
-    plan = planner.get_evacuation_plan(data['query'], data['user_needs'])
+    query = data.get('query', '')
+    needs = data.get('needs', 'none')
+    plan = planner.get_evacuation_plan(query, needs)
     return jsonify({'plan': plan})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
